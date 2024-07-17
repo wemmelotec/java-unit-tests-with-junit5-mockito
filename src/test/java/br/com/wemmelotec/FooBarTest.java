4551107d03c20a61ffd8f29db3ca2ec551d01b77
Wellington <tonmelo@hotmail.com>
2024-07-17 19:42:18 -0300

    aula 14

---
+++ src/test/java/br/com/wemmelotec/FooBarTest.java
@@ -0,0 +1,19 @@
+package br.com.wemmelotec;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class FooBarTest {
+
+	@Test
+	void test() {
+		
+		//primeiro comando que vai gerar uma falha
+		//fail("Not yet implemented");
+		
+		//segundo comando que não vai ter falha, apenas o sysout
+		System.out.println("Junit");
+	}
+
+}
